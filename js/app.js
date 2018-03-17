7184105d2acb518f7b1655c380a6923bfe731b9c
Linjing <yanglinjing@gmail.com>
2018-03-18 01:00:26 +1100

    Completed the whole Match function!

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const symbols = ['fa-anchor', 'fa-bicycle', 'fa-bolt', 'fa-bomb', 'fa-cube', 'fa
 shuffle(symbols);//洗牌
 
  /* 让符号按随机顺序显示在卡片上 */
-const picList = document.querySelectorAll('.card i');//选择16个i元素
+const picList = document.querySelectorAll('.card i');//选择16个i元素,nodeList
 for (let i = 0; i < picList.length; i++) {//为所有i元素设置class
   let symbol = 'fa ' + symbols[i];
   picList[i].setAttribute('class', symbol);
@@ -13,27 +13,56 @@ for (let i = 0; i < picList.length; i++) {//为所有i元素设置class
 /*
  * 设置一张卡片的事件监听器。 如果该卡片被点击：
  *  - 显示卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
+ *
  *  - 将卡片添加到状态为 “open” 的 *数组* 中（将这个功能放在你从这个函数中调用的另一个函数中）
  *  - 如果数组中已有另一张卡，请检查两张卡片是否匹配
  *    + 如果卡片匹配，将卡片锁定为 "open" 状态（将这个功能放在你从这个函数中调用的另一个函数中）
  *    + 如果卡片不匹配，请将卡片从数组中移除并隐藏卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
+ *
  *    + 增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）
  *    + 如果所有卡都匹配，则显示带有最终分数的消息（将这个功能放在你从这个函数中调用的另一个函数中）
  */
 
 //事件代理event delegation：监视父元素ul，管理子元素li的事件
-const deck = document.querySelector('.deck');
-deck.addEventListener('click', respondClick);
+document.querySelector('.deck').addEventListener('click', respondClick);//父元素ul
+let open = [];
+let card2;
 function respondClick(evt){
-    if (evt.target.nodeName === 'LI'){//nodeName要大写
-        evt.target.setAttribute('class', 'card open show');
+    let card = evt.target;//子元素li;
+    if (card.nodeName === 'LI' && !card.classList.contains('match')){//nodeName要大写
+        card.setAttribute('class', 'card open show');//为li设置class
+
+        let symbol = card.firstElementChild.className;//i的class
+        addToOpen(symbol);
+        if(open.length===1){
+            card2 = card;
+        }else if(open.length===2){
+            compare(card);
+        }
     }
 }
 
+function addToOpen(symbol){
+    if(open.length<2){
+        open.push(symbol);
+    }
+}
 
-
-
-
+function compare(card){
+    if(open[0]===open[1]){
+        card.setAttribute('class', 'card match');//为li设置class
+        card2.setAttribute('class', 'card match');
+    }
+    if(open[0]!=open[1]){
+        card.setAttribute('class', 'card wrong');//为li设置class
+        card2.setAttribute('class', 'card wrong');
+        setTimeout(function(){
+            card.setAttribute('class', 'card');//为li设置class
+            card2.setAttribute('class', 'card');
+        }, 1000);
+    }
+    open = [];
+}
 
 
 
