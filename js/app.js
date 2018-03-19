2b84eac0e5adcf1f1c91d7366f17b9d8f97e9460
yanglinjing@gmail.com <yanglinjing@gmail.com>
2018-03-19 22:33:30 +1100

    Add fireworks

--- js/app.js
+++ js/app.js
@@ -127,6 +127,7 @@ function congrt(){
     document.getElementById('congratulations').setAttribute('class', 'showWindow');
     document.querySelector('#congratulations .moves').textContent = moves;
     document.querySelector('#congratulations .stars').textContent = starNum;
+    runFirework();
 }
 
 
@@ -141,3 +142,66 @@ function reload(){
 document.getElementById('close').addEventListener('click', function(){
     document.getElementById('congratulations').setAttribute('class', 'hideWindow');
 });
+
+
+
+/*
+ *烟花特效
+ *来自:http://blog.csdn.net/wcslb/article/details/53142221
+ */
+
+
+//封装一个颜色随机的效果
+function randomColor(){
+    var color = "rgb("
+    var r = parseInt(Math.random()*256);
+    var g = parseInt(Math.random()*256);
+    var b = parseInt(Math.random()*256);
+    color = color+r+","+g+","+b+")";
+    return color;
+}
+//创建一个制造烟花的构造函数，第一个参数为元素，第二参数为初始x轴位置，第三参数为y轴位置。
+function Fireworks(Div,x,y){
+    Div.style.backgroundColor=randomColor();   //给烟花添加背景色
+    Div.className="firworks";                   //添加一个class
+    document.body.appendChild(Div);
+    Div.style.left=x+"px";                      //把鼠标点击坐标给div
+    Div.style.top=y+"px";
+    var speedX = (parseInt(Math.random()*2) == 0 ? 1 : -1)*parseInt(Math.random()*16 + 1);  //三目运算符随机移动方向，概率50%,为1时往正方向移动，负1时往反方向移动第二个随机数随机速度快慢
+    var speedY = (parseInt(Math.random()*2) == 0 ? 1 : -1)*parseInt(Math.random()*20 + 1);
+    this.move=function(){
+        var i = 3;
+        var time1=setInterval(function(){
+            i++;
+            Div.style.left=Div.offsetLeft+speedX+"px";
+            Div.style.top=Div.offsetTop+speedY+i+"px";   //当i+speedY>0时,烟花朝下运动。
+            if(Div.offsetLeft+Div.offsetWidth>window.innerWidth|| Div.offsetLeft<2 || Div.offsetTop+Div.offsetHeight>window.innerHeight || Div.offsetTop<2 ){
+                Div.remove();       //移动出可视区域记得删除div和清除定时器
+                clearInterval(time1);
+            }
+        },30);
+    }
+}
+
+function runFirework(){
+    let x=300, y=300;
+    for(let i=0;i<80;i++){       //随机烟花的数量
+        const div=document.createElement("div");
+        let b=new Fireworks(div, x, y);
+        b.move();
+        locaion(x, y);
+    }
+}
+
+//生成网页随机坐标：来源http://blog.csdn.net/dyushuo6230/article/details/7285313
+function locaion(x, y){
+
+    //右下边求出图片起始点最大X和Y的坐标值
+    const w=window.screen.availWidth
+    const h=window.screen.availHeight;
+
+    //随机产生坐标x和坐标Y的值（都是从零到起始点最大X和Y的坐标值）
+    x=Math.round(Math.random()*(w));
+    y=Math.round(Math.random()*(h));
+    return x, y;
+}
